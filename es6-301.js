395f59c6cd84be45b180b29aa9070b9ce622eb66
plmn65410 <69616757+plmn65410@users.noreply.github.com>
2020-12-01 14:34:16 +0700

    Add files via upload

---
+++ es6-301.js
@@ -0,0 +1,19 @@
+// The String
+//1.The basic Syntax of String
+// let message = "Hello world";
+// console.log(message);
+//2.The basic Syntax of String
+let fname = "Mark";
+let lname = "Sakaberg";
+let fullName = fname + " " + lname;
+// console.log(fname + " " + lname);
+console.log(fullName);
+//3.The String with numder
+console.log("Hello" + 12)//Hello12
+console.log("10" + 2)//102
+
+
+console.log("10" == 10);//true
+console.log("10" === 10); //false
+console.log("10" !== 10);//
+
